82e65d680dfb80b5cff5e709503c343fc35457ab
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-11-18 18:46:21 +0200

    Update all

--- dst/start-server.sh
+++ dst/start-server.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# SERVER opotions
-# final server location is
-# $root/$conf_dir/$cluster
-root="$HOME/dstservers"
-conf_dir="clusters" 
+root="$(pwd)"
+clustersDir="clusters" 
 cluster="dcluster"
 shard="Master"
 additional=("-console")
+dstDir="$HOME/steamapps/DST"
 # additional=("-offline -disabledatacollection -console")
+ 
+if [ -e ~/.dstserver.sh ]; then
+	source ~/.dstserver.sh
+fi
+
+if [ -e "$root/dstserver.sh" ]; then
+	source "$root/dstserver.sh"
+fi
 
 while [[ $# -gt 0 ]]; do
 	case $1 in
@@ -24,8 +30,12 @@ while [[ $# -gt 0 ]]; do
 			root="$2"
 			shift; shift
 			;;
-		-d|--config-dir)
-			conf_dir="$2"
+		-d|--clusters-dir)
+			clustersDir="$2"
+			shift; shift
+			;;
+		-i|--dst-dir)
+			dstDir="$2"
 			shift; shift
 			;;
 		-o|--server-option)
@@ -35,22 +45,33 @@ while [[ $# -gt 0 ]]; do
 		-h|--help)
 			cat << EOF
 USAGE: $0 [OPTIONS]
+Start Don't Starve Together server.
 
 Available options:
--c --cluster [CLUSTER PATH]    Cluster name to run.
-   default: $cluster
--s --shard [SHARD PATH]        Shard to start (Caves, Master etc.).
-   default: $shard
--r --root [PATH]               Path to the clusters directory.
-   default: $root
--o --server-option [OPTIONS]   Additional nullrenderer options.
-   default: $additional
--d --config-dif [PATH]         Clusters home directory.
-   default: $conf_dir
--h --help                      Show this message.
+-c --cluster [CLUSTER PATH] def: $cluster
+    Cluster name to run.
+-s --shard [SHARD PATH] def: $shard
+    Shard to start (Caves, Master etc.).
+-r --root [PATH] def: $root
+    Path to the clusters directory.
+-o --server-option [OPTIONS] def: $additional
+    Additional nullrenderer options.
+-d --clusters-dir [PATH] def: $clustersDir
+    Clusters home directory.
+-i --dst-dir [PATH] def: $dstDir
+    Don't Starve Together installation directory.
+-h --help
+    Show this message.
 
-Clusters are in <root>/<config-directory>/
-Active cluster is in <root>/<config-directory>/<cluster>
+Clusters are in <root>/<config-directory>/.
+Active cluster is <root>/<config-directory>/<cluster>.
+Don't Starve server in <dst-dir>.
+
+You can override default option values 
+by putting overrides in ~/.dstserver.sh and <root>/dstsever.sh.
+For example, to chande default --cluster-dir value, put 
+   clusterDir=<newValue>
+in ~/.dstsever.sh or <root>/dstsever.sh.
 EOF
 			exit 0
 			;;
@@ -68,8 +89,7 @@ EOF
 done
 
 # INSTALL setup
-install_dir="$HOME/steamapps/DST"
-dontstarve_dir="$root/$conf_dir"
+dontstarve_dir="$root/$clustersDir"
 
 function fail() {
     echo Error: "$@" >&2
@@ -93,7 +113,7 @@ cd "$install_dir/bin64" || fail
 run_shared=(./dontstarve_dedicated_server_nullrenderer_x64)
 run_shared+=(-persistent_storage_root "$root")
 run_shared+=(-cluster "$cluster")
-run_shared+=(-conf_dir "$conf_dir")
+run_shared+=(-conf_dir "$clustersDir")
 run_shared+=(-monitor_parent_process $$)
 run_shared+=("$additional")
 
