190b73be9722573d851124fa61a279971db99232
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-11-18 18:13:49 +0200

    Make everything to curl aroud $root

--- minecraft/mcserver.sh
+++ minecraft/mcserver.sh
@@ -3,6 +3,7 @@
 serversDirectory="servers"
 server="dserver"
 
+root=$(pwd)
 serverJar="minecraft_1.21.3"
 jarsDirectory="serverjars"
 jarOptions="nogui"
@@ -16,8 +17,8 @@ if [ -e ~/.mcserver.sh ]; then
 	source ~/.mcserver.sh
 fi
 
-if [ -e mcserver.sh ]; then
-	source ./mcserver.sh
+if [ -e "$root/mcserver.sh" ]; then
+	source "$root/mcserver.sh"
 fi
 
 function fail() {
@@ -55,6 +56,10 @@ while [[ $# -gt 0 ]]; do
 			jarsDirestory="$2"
 			shift; shift
 			;;
+		-r|--root)
+			root="$2"
+			shift; shift
+			;;
 		--java-options)
 			javaOptions="$2"
 			shift; shift
@@ -77,34 +82,40 @@ USAGE: $0 [OPTIONS]
 Starts minecraft server
 
 Avaliable options:
--d --servers-directory [PATH]  Directory where all servers located.
-   default: $serversDirectory
--s --server [SERVER NAME]      Server to start.
-   default: $server
--e --server-jar [FILENAME]     Server jar file. Must be in --jars-directory.
-   default: $serverJar
--j --jars-directory [PATH]     Path to look for serve executables.
-   default: $jarsDirectory
---java-options [OPTIONS]       Additional java options.
-   default: $javaOptions
---jar-options [OPTIONS]        Additional server jar options.
-   default: $jarOptions
---start-heap [UINT]            Starting heap size in mb.
-   default: $startHeap
---max-heap [UINT]              Maximum heap size in mb.
-   default: $maxHeap
--h --help                      Show this message.
+ -d --servers-directory [PATH] def: $serversDirectory
+     Servers collection directory name in <root>/. 
+ -s --server [FOLDER NAME] def: $server
+     Server directory to start. 
+		 Should be in <root>/<servers-directory>/.
+ -e --server-jar [FILENAME] def: $serverJar
+     Server jar file. Must be in <root>/<jars-directory>.
+ -j --jars-directory [PATH] def: $jarsDirectory
+     Path to look for server executables.
+		 Should be in <root>/.
+ -r --root [PATH] def: $jarsDirectory
+     Path to server root folder. This folder
+		 contains servers jar folders and servers itsef.
+ --java-options [OPTIONS] def: $javaOptions
+     Additional java options.
+ --jar-options [OPTIONS] def: $jarOptions
+     Additional server jar options.
+ --start-heap [UINT] def: $startHeap
+     Starting heap size in mb.
+ --max-heap [UINT] def: $maxHeap
+     Maximum heap size in mb.
+ -h --help                     
+     Show this message.
 
 Running sever is in <servers-directory>/<server>.
 Sever is at <jars-directory>/<server-jar>.
 
 You can add option defaults to ~/.mcserver.sh or 
-mcserver.sh in current folder with defaults. They will be sourced with 
-mentioned order.
+<root>/mcserver.sh in folder with defaults. 
+They will be sourced with mentioned order.
 For example, to redefine default --start-heap 
 value to 2048, add
-startHeap=2048
-to the mcserver.sh or ~/.mcserver.sh.
+	startHeap=2048
+to the <root>/mcserver.sh or ~/.mcserver.sh.
 EOF
 			exit 0
 			;;
@@ -121,10 +132,11 @@ EOF
 	esac
 done
 
-check_for_file "$serversDirectory" "no servers directory"
-check_for_file "$serversDirectory/$server" "no server folder"
+check_for_file "$root" "no server root"
+check_for_file "$root/$serversDirectory" "no servers directory"
+check_for_file "$root/$serversDirectory/$server" "no server folder"
 
-check_for_file "$jarsDirectory" "no server java archive directory"
+check_for_file "$root/$jarsDirectory" "no server java archive directory"
 
 check_uint "$startHeap" "--start-heap"
 check_uint "$maxHeap" "--max-heap"
@@ -132,8 +144,8 @@ if [ $startHeap -gt $maxHeap ]; then
 	fail "starting RAM size is larger that allowed size"
 fi
 
-jar="$(pwd)/$jarsDirectory/$(basename "$serverJar" .jar).jar"
-check_for_file "$jar" "no server java archive"
+jar="$root/$jarsDirectory/$(basename "$serverJar" .jar).jar"
+check_for_file "$jar" "no server java archive \"$serverJar[.jar]\""
 
-cd "$serversDirectory/$server"
+cd "$root/$serversDirectory/$server"
 java -Xms"$startHeap"M -Xmx"$maxHeap"M $javaOptions -jar $jar $jarOptions
